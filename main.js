4a7e3e5e3707c1b147c881ef21fd8eb22eddc4cd
latscap <sveshnikovaka@gmail.com>
2023-12-07 18:17:43 +0300

    lesson 3 - finish events - simple todo

--- main.js
+++ main.js
@@ -1,2 +1,21 @@
-document.getAnimations('output')
-.innertext = "welcome Leon Kennedy"
+//document
+//.getElementById('output')
+//.innerText = 'Welcome';
+
+const domOutput = document.getElementById('output');
+const domInput = document.getElementById('input');
+const domButton = document.getElementById('enter');
+
+// domOInput.value = '123';
+// domOutput.innerText = 'Welcome';
+
+domInput.oninput = function(event) {
+  console.log('oninput', event);
+  // domOutput.innerText = 'Hello, ' + event.target.value;
+}
+
+domButton.onclick = function(event) {
+  console.log('onclick', event);
+  domOutput.innerHTML = domOutput.innerHTML + <div>${domInput.value}</div>;
+} 
+
